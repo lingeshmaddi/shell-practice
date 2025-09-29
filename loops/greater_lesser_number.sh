74058bc18d360e484a6f3a39052cdf8ce7b611be
lingesh <lingeshmaddi@gmail.com>
2025-09-29 14:45:25 +0530

    greter than or leethan

---
+++ loops/greater_lesser_number.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "enter number"
+
+number= read $1
+
+if (number lt 10) then
+
+echo "entered number :$number is  less tahn 10"
+
+else
+
+echo "entered number : $number is greater than 10"
+
+fi
